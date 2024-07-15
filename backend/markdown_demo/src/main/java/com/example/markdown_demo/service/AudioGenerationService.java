822cb8655f23453180328ca273016a82f7371def
hongshangqiu <1151123638@qq.com>
2024-07-15 15:37:13 +0800

    Revert "语音转文字接口"
    This reverts commit c3ba1fe7a467660250100e8ae7853e862fe00e5d.

--- backend/markdown_demo/src/main/java/com/example/markdown_demo/service/AudioGenerationService.java
+++ backend/markdown_demo/src/main/java/com/example/markdown_demo/service/AudioGenerationService.java
@@ -1,7 +1,6 @@
 package com.example.markdown_demo.service;
 
 import com.example.markdown_demo.common.dto.AudioRequestDTO;
-import org.springframework.web.multipart.MultipartFile;
 
 public interface AudioGenerationService {
     byte[] generateAudio(AudioRequestDTO request) throws Exception;
--- backend/markdown_demo/src/main/java/com/example/markdown_demo/service/AudioToTextService.java
+++
@@ -1,8 +0,0 @@
-package com.example.markdown_demo.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface AudioToTextService {
-    String processAudioFile(MultipartFile file)throws Exception;
-
-}
